38abe6101bc9649eac5a9005fe179b4db93c859b
Arsam <91605508+arsam-sedighi@users.noreply.github.com>
2023-01-14 00:57:03 +0330

    Update JLMath
    [Rework] rename allcate method to divide

--- Source/JLMath/JLMath.java
+++ Source/JLMath/JLMath.java
@@ -7,22 +7,27 @@ package com.javalaboratory.JLMath;
 
 @SuppressWarnings("all")
 public class JLMath {
+    // This method adds two numbers together
     public static double add(double firstNumber, double secondNumber) {
         return (firstNumber + secondNumber);
     } // end method add
 
+    // This method subtracts two numbers
     public static double subtract(double firstNumber, double secondNumber) {
         return (firstNumber - secondNumber);
     } // end method subtract
 
+    // This method multiplies two numbers together
     public static double multiply(double firstNumber, double secondNumber) {
         return (firstNumber * secondNumber);
     } // end method multiply
 
-    public static double allcate(double firstNumber, double secondNumber) {
+    // This method divides two numbers
+    public static double divide(double firstNumber, double secondNumber) {
         return (firstNumber / secondNumber);
-    } // end method allcate
+    } // end method divide
 
+    // This method exponentiates two numbers
     public static double power(double firstNumber, double secondNumber) {
         double result = 1;
         for (int counter = 1; counter <= secondNumber; counter++) {
@@ -31,6 +36,7 @@ public class JLMath {
         return result;
     } // end method power
     
+    // This method compares two numbers
     public static double compare(double firstNumber, double secondNumber) {
         if (firstNumber > secondNumber) {
             return firstNumber;
@@ -38,11 +44,11 @@ public class JLMath {
         else { return secondNumber; }
     } // end method compaire
     
+    // This method adds a sequence of numbers together
     public static double range(double number) {
         if (number > 0) {
             return number + range(number - 1);
         }
         else { return 0; }
     } // end method range
-
 } // end class JLMath
