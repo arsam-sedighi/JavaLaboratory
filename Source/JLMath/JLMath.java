090200f4cd9c57361835c9f0605244bf408e0c2b
Arsam <91605508+arsam-sedighi@users.noreply.github.com>
2023-01-14 21:34:38 +0330

    Update JLMath
    [Update] Add isEven & isOdd method to JL Math package.
    - These methods determine whether the number is odd or even
    [Bugfix] From now on, by entering negative numbers into the range method, instead of returning zero, the number will be automatically converted to a positive number and the calculation process will continue.

--- Source/JLMath/JLMath.java
+++ Source/JLMath/JLMath.java
@@ -46,9 +46,26 @@ public class JLMath {
     
     // This method adds a sequence of numbers together
     public static double range(double number) {
+        if (number < 0) { number *= -1; }
         if (number > 0) {
             return number + range(number - 1);
         }
         else { return 0; }
     } // end method range
+    
+    // This method determines whether the number is even or not
+    public static boolean isEven(double number) {
+        if (number % 2 == 0) {
+            return true;
+        }
+        else { return false; }
+    } // end method isEven
+
+    // This method determines whether the number is odd or not
+    public static boolean isOdd(double number) {
+        if (number % 2 == 1) {
+            return true;
+        }
+        else { return false; }
+    } // end method isOdd
 } // end class JLMath
