7365d5e00e64642e93bd8b3a5c0d4f3879994b6f
Arsam <91605508+arsam-sedighi@users.noreply.github.com>
2023-02-04 00:44:08 +0330

    Update JLHealth.java

--- Source/JLHealth/JLHealth.java
+++ Source/JLHealth/JLHealth.java
@@ -12,26 +12,4 @@ public class JLHealth {
         double result = weight / JLMath.power(stature, 2);
         return result;
     } // end method BMI
-
-    // This method determines the health status of the body mass index
-    public  static String BMIHealthCondition(double BMINumber) {
-        final double UNDER_WEGHT = 18.5;
-        final double NORMAL = 24.9;
-        final double OVER_WEIGHT = 29.9;
-        final double OBESE = 30;
-        String result = "";
-        if (BMINumber <= UNDER_WEGHT) {
-            result = "Underweight";
-        }
-        if (BMINumber <= NORMAL) {
-            result = "Normal";
-        }
-        if (BMINumber >= OVER_WEIGHT) {
-            result = "Overweight";
-        }
-        if (BMINumber >= OBESE) {
-            result = "Obese";
-        }
-        return result;
-    } // end method BMIHealthCondition
 } // end class JLHealth
